943812dbd5429090c11837b28f123d666fc4beab
petchgnz (desktop) <phetchgnz@gmail.com>
2026-07-18 21:30:38 +0700

    fix: auth when init

--- apps/frontend/src/App.vue
+++ apps/frontend/src/App.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-// import { useAuth } from './composables/useAuth'
+import { useAuth } from './composables/useAuth'
 
-// const { initAuth } = useAuth()
-// initAuth() // restore sessions when refresh
+const { initAuth } = useAuth()
+initAuth() // restore sessions when refresh
 </script>
 
 <template>
-  <!-- <h1>Hello</h1> -->
   <RouterView />
 </template>
